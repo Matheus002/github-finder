514645f6b9115ea088d81a7285103cdf93fe6206
Math002 <matheus002_@hotmail.com>
2020-03-05 08:09:12 -0300

    add Spinner e refatoração de classes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = (props) => {
+const Navbar = ({ icon, titulo}) => {
     return (
         <nav className='navbar bg-primary'>
             <h1>
-                <i className={props.icon} /> {props.titulo}
+                <i className={icon} /> {titulo}
             </h1>
         </nav>
-    )
-}
+    );
+};
 
 Navbar.defaultProps = {
     titulo: 'Buscador Git',
